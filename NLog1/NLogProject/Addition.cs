7a24a98e2e14799832e01bf4eddf8fce8d175614
anandsaikumar <anandsai298@gmail.com>
2023-03-17 11:17:16 +0530

    [sai] Add : Nlog main where log files functionality

---
+++ NLog1/NLogProject/Addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    public class Addition
+    {
+        Nlog nlog=new Nlog();
+        public void sum(int a,int b)
+        {
+            if (a==0 || b==0)
+            {
+                nlog.LogInfo("Info:sum()");
+                nlog.LogWarn("Warn:a or b is 0");
+                nlog.LogError("Error:either a or b is 0");
+            }
+            int c = a + b;
+            nlog.LogInfo("Info:sum()");
+            nlog.LogDebug("Debug:sum()" + c);
+        }
+    }
+}
---
+++ NLog1/NLogProject/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLogProject;
+using System;
+namespace LineComputation
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Addition addition = new Addition();
+            addition.sum(0, 10);
+        }
+    }
+}
